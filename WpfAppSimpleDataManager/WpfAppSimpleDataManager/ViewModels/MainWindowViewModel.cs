4973f7f1aa5831fa7ee3b8448a9d024a1bfe44c3
Mephisto Lin <Mephisto007linlin@outlook.com>
2025-05-31 15:05:40 +0800

    feat: 新增主視窗介面

---
+++ WpfAppSimpleDataManager/WpfAppSimpleDataManager/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace WpfAppSimpleDataManager.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        // 如有需要，可以在此統一管理兩個子 ViewModel 的共享資源
+        // 本範例不特別實作，僅保留後續擴充空間
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void RaisePropertyChanged(string propName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+    }
+}
---
+++ WpfAppSimpleDataManager/WpfAppSimpleDataManager/Views/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using Wpf.Ui.Controls;
+using WpfAppSimpleDataManager.ViewModels;
+
+namespace WpfAppSimpleDataManager.Views
+{
+    public partial class MainWindow : FluentWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // 設定 MainWindow 的 DataContext
+            this.DataContext = new MainWindowViewModel();
+        }
+    }
+}
